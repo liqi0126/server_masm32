9172b5280d6da99bc9a8a972374391fff00720d8
liqi17-thu <624275030@qq.com>
2020-11-06 19:55:21 +0800

    my logic

--- server/main.asm
+++ server/main.asm
@@ -31,94 +31,84 @@ BACKLOG		EQU		5
 
 IDC_COUNT EQU 40002
 
-CONNECT_LOGIN	EQU		48 ; ASCII '0'
-CONNECT_SIGNUP	EQU		49 ; ASCII '1'
+CLIENT_SIGNUP		EQU		48 ; ASCII '0'
+CLIENT_LOGIN		EQU		49 ; ASCII '1'
+CLIENT_ROOM_TALK	EQU		50 ; ASCII '2'
+CLIENT_1TO1_TALK	EQU		51 ; ASCII '3'
+CLIENT_ADD_FRIEND	EQU		52 ; ASCII '4'
+CLIENT_FRI_REPLY	EQU		53 ; ASCII '5'
+CLIENT_DEL_FRIEND	EQU		54 ; ASCII '6'
 
-CLIENT_SEND_MSG		EQU		49 ; ASCII '1'
-CLIENT_ADD_FRIEND	EQU		50 ; ASCII '2'
-CLIENT_DEL_FRIEND	EQU		51 ; ASCII '3'
-CLIENT_SEND_FIG		EQU		52 ; ASCII '4'
+SERVER_INVALID			EQU		-1
+SERVER_FAIL				EQU		0
+SERVER_SUCCESS			EQU		1
+SERVER_ROOM_TALK		EQU		2
+SERVER_1TO1_TALK		EQU		3
+SERVER_FRIEND_APPLY		EQU		4
+SERVER_FRIEND_LIST		EQU		5
+SERVER_FRIEND_NOTIFY	EQU		6
 
-SERVER_SEND_MSG			EQU		1
-SERVER_SEND_FIG			EQU		2
-SERVER_ADD_FRIEND		EQU		3
-SERVER_DEL_FRIEND		EQU		4
-SERVER_INVALID			EQU		5
+FRIEND_ONLINE	EQU		0
+FRIEND_OFFLINE	EQU		1
+FRIEND_PASS		EQU		2
+FRIEND_REJECT	EQU		3
+FRIEND_DEL		EQU		4
 
-SERVER_BEC_FRIEND_ONLINE	EQU	0
-SERVER_BEC_FRIEND_OFFLINE	EQU 1
-
-SERVER_FRIEND_ONLINE	EQU		4
-SERVER_FRIEND_OFFLINE	EQU		5
+IS_FRIEND_ASCII			EQU		48 ; ASCII '0'
+FRIEND_ONLINE_ASCII		EQU		49 ; ASCII '1'
+FRIEND_OFFLINE_ASCII	EQU		50 ; ASCII '2'
 
 ;==================== DATA =======================
 .data
 ; message
 BIND_PORT_HINT		db "BIND PORT:", 0
 START_HINT			db "SERVER START!", 0dh, 0ah, 0
-NEW_CONNECT_HINT	db "NEW CONNECT ATTEMPT", 0ah, 0dh, 0
 
-LOGIN_SUCCESS_HINT	db "LOGIN SUCCESS", 0ah, 0dh, 0
-LOGIN_FAIL_HINT		db "LOGIN FAIL", 0ah, 0dh, 0
-SIGNUP_SUCCESS_HINT db "SIGNUP SUCCESS", 0ah, 0dh, 0
-SIGNUP_FAIL_HINT	db "SIGNUP FAIL", 0ah, 0dh, 0
+SUCCESS_HINT		db "1", 0
+SIGNUP_SUCCESS_HINT db "USER %s SIGNUP", 0ah, 0dh, 0
+SIGNUP_FAIL_HINT	db "UsER %s SIGNUP FAIL", 0ah, 0dh, 0
+LOGIN_SUCCESS_HINT	db "USER %s LOGIN", 0ah, 0dh, 0
+LOGIN_FAIL_HINT		db "USER %s LOGIN FAIL", 0ah, 0dh, 0
+
 
 ERR_BUILD_SOCKET	db "Fail to Open Socket", 0
 ERR_BIND_SOCKET		db "Fail to Bind Socket", 0
+ERR_REPEAT_SIGNIN	db "User %s already sign in", 0
+ERR_WRONG_PASS		db "Password is wrong", 0
+ERR_NO_SUCH_USER	db "No User %s, please sign in firstly", 0
+ERR_REPEAT_LOGIN	db "User %s already login", 0
 
-SUCCESS_HINT		db "success", 0
-FAIL_HINT			db "fail", 0
 
-INPUT_DB_FORMAT		db "%d", 0
+MSG_FORMAT0			db "%d", 0
 MSG_FORMAT1			db "%d %s", 0
 MSG_FORMAT2			db "%s%s %d ", 0
 MSG_FORMAT3			db "%d %s %s", 0
 MSG_FORMAT4			db "%d %s %d", 0
 
-
-DEBUG_PARSE_FRIENDLIST db "parse results:%s", 0ah, 0dh, 0
-
-; server
-serverPort dw ?
-listenSocket dd  ?
-
-szConnect db "连接",0
- 
-szDisConnect db "断开",0
-
-typeCodeZero db "0", 0
-typeCodeOne db "1", 0
-typeCodeTwo db "2", 0
-typeCodeThree db "3", 0
-typeCodeFour db "4", 0
-
 ; thread
 dwThreadCounter dd ?
-dwFlag dd ?
-F_STOP dd ?
 hWinMain dd ?
 
-
-; write connection socket for each client
-connSocket dd 20 DUP(?)
-
-loginSuccess db "success", 0
-loginFailure db "fail", 0
-
-msgFormat1 db "%s %s %s", 0
-msgFormat2 db "%s %s %s %s", 0
-msgFormat3 db "%d %s", 0
-msgFormat4 db "%s%s", 0
-msgFormat5 db "%s %d ", 0
-msgFormat6 db "%s%s", 0
-
-clientlist client 100 DUP(<>)
+; connect client
+clientlist client 256 DUP(<>)
 clientnum dd 0
 
 addFriendFail db "6 fail", 0
 
 ;=================== CODE =========================
 .code
+
+;--------------------------------------------------------------
+getArrayEleByNum PROC arrayPtr:dword, eleSize:dword, Num:dword
+; eax = array[Num]
+;--------------------------------------------------------------
+	mov eax, eleSize
+	mov ebx, Num
+	mul ebx
+	add eax, arrayPtr
+	ret
+getArrayEleByNum ENDP
+
 
 ;--------------------------------------------------------------
 getClientId PROC uses ebx username:ptr byte
@@ -260,38 +250,53 @@ addNewFriend PROC user1:ptr byte, user2:ptr byte
 addNewFriend ENDP
 
 ;--------------------------------------------------------------
-parseFriendList PROC friendlist:ptr byte, msgField:ptr byte
-; friendlist(input): FRIEND1 FRIEND2 FRIEND3 ...
-; msgField(output): FRIEND1 on/off FRIEND2 on/off ...
+checkFriendOnOffLine PROC friendList:ptr byte
+; check friend online or offline
+; friendList(input): FRIEND1:status1 FRIEND2:status2 ...
+; updatedFriendList(output): FRIEND1:updatedStatus1 FRIEND2:updatedStatus2 ...
+; origin: 0:friend 3:pending 4:deleted
+; new: 1:online 2:offline 3:pending 4:deleted
 ;--------------------------------------------------------------
-	LOCAL @tmpUsername:dword
-	LOCAL @tmpFd:dword
+	LOCAL @cursor:dword
+	LOCAL @len:dword
+	LOCAL @username[256]:byte
+	LOCAL @friendType:byte
+	LOCAL @sockfd:dword
 
-	invoke crt_strcat, friendlist, offset SEP
+	mov eax, friendList
+	mov @cursor, eax
 
+	invoke crt_strcat, friendList, offset SEP
 	.while 1
-		invoke crt_strstr, friendlist, offset SEP
+		invoke crt_strstr, @cursor, offset SEP
 		.if eax == 0
 			jmp finish
 		.endif
+		dec eax
+		mov bl, [eax]
+		mov @friendType, bl
 
-		mov bl, 0
-		mov [eax], bl
-		mov ebx, friendlist
-		mov @tmpUsername, ebx
+		mov @len, eax
+		add @len, -1
+		mov ebx, @cursor
+		sub @len, ebx
+		invoke RtlZeroMemory, addr @username, 256
+		invoke crt_strncpy, addr @username, @cursor, @len
 
-		push eax
-		invoke getClientFd, @tmpUsername, addr @tmpFd
-		invoke crt_sprintf, msgField, offset MSG_FORMAT2, msgField, @tmpUsername, eax
-		pop eax
+		.if @friendType == IS_FRIEND_ASCII
+			invoke getClientFd, addr @username, addr @sockfd
+			invoke crt_printf, offset msgFormat3, @friendType, addr @username
+		.endif
 
-		mov friendlist, eax
-		inc friendlist
+
+		mov eax, @len
+		add eax, 3
+		add @cursor, eax
 	.endw
 
 finish:
 	ret
-parseFriendList ENDP
+checkFriendOnOffLine ENDP
 
 ;--------------------------------------------------------------
 broadcastOnOffLine PROC uses ebx currentname:ptr byte, isOn:dword
@@ -396,17 +401,18 @@ serviceThread PROC uses ebx clientid:dword
 	LOCAL @targetSockfd:dword
 	LOCAL @msgContent:ptr byte
 	LOCAL @msgField:ptr byte
-	LOCAL _hSocket:DWORD
+	LOCAL @sockfd:DWORD
 	LOCAL @friendlist[1024]:byte
 	mov @szBuffer, alloc(BUFSIZE)
 	mov @msgField, alloc(BUFSIZE)
 	mov @msgContent, alloc(BUFSIZE)
 
+	; get sockfd and username
 	mov eax, clientid
 	mov ebx, type client
 	mul ebx
 	mov edx, clientlist[eax].sockfd
-	mov _hSocket, edx
+	mov @sockfd, edx
 	invoke crt_strcpy, addr @currentUsername, addr clientlist[eax].username
 
 	; read and send friend lists to current user
@@ -415,7 +421,7 @@ serviceThread PROC uses ebx clientid:dword
 	invoke RtlZeroMemory, @msgField, BUFSIZE
 	invoke parseFriendList, addr @friendlist, @msgField
 	invoke crt_strlen, @msgField
-	invoke send, _hSocket, @msgField, eax, 0
+	invoke send, @sockfd, @msgField, eax, 0
 
 	; tell his friend that he is on/offline
 	invoke broadcastOnOffLine, addr @currentUsername, 1
@@ -425,7 +431,7 @@ serviceThread PROC uses ebx clientid:dword
 
 	.while 1
 		mov @stFdset.fd_count,1
-		push _hSocket
+		push @sockfd
 		pop @stFdset.fd_array
 		mov @stTimeval.tv_usec,200*1000 ;ms
 		mov @stTimeval.tv_sec,0
@@ -436,7 +442,7 @@ serviceThread PROC uses ebx clientid:dword
 		.endif
 		.if eax
 			invoke RtlZeroMemory, @szBuffer, BUFSIZE
-			invoke recv, _hSocket, @szBuffer, BUFSIZE, 0
+			invoke recv, @sockfd, @szBuffer, BUFSIZE, 0
 			.break  .if eax == SOCKET_ERROR
 			.break  .if !eax
 			; parse client commend
@@ -460,14 +466,14 @@ serviceThread PROC uses ebx clientid:dword
 			.elseif eax == SERVER_ADD_FRIEND
 				invoke addNewFriend, addr @currentUsername, @msgContent
 				.if eax == 0
-					invoke send, _hSocket, addr addFriendFail, sizeof addFriendFail, 0
+					invoke send, @sockfd, addr addFriendFail, sizeof addFriendFail, 0
 				.endif
 			.elseif eax == SERVER_DEL_FRIEND
 				; TBD
 			.endif
 		.endif
 	.endw
-	invoke closesocket, _hSocket
+	invoke closesocket, @sockfd
 	dec dwThreadCounter
 	; 从当前用户列表更改该下线用户状态
 	mov eax, clientid
@@ -484,46 +490,53 @@ serviceThread PROC uses ebx clientid:dword
 	ret
 serviceThread ENDP
 
-
+;--------------------------------------------------------------
 logIn PROC sockfd:dword, username:ptr byte, password:ptr byte
-	LOCAL @tempfd:dword
-
-	; check whether this username exists
+; user log in
+;--------------------------------------------------------------
+	LOCAL @tmpMsg[512]:byte
 	invoke ifSignIn, username
 	.if eax == 0
-		invoke send, sockfd, addr FAIL_HINT, sizeof FAIL_HINT, 0
+		invoke crt_sprintf, addr @tmpMsg, offset MSG_FORMAT1, SERVER_FAIL, offset ERR_NO_SUCH_USER
+		invoke crt_strlen, addr @tmpMsg
+		invoke send, sockfd, addr @tmpMsg, eax, 0
+		mov eax, -1
+		ret
+	.endif
+
+	; repeat login
+	invoke getClientId, username
+	.if eax == 1
+		invoke crt_sprintf, addr @tmpMsg, offset MSG_FORMAT1, SERVER_FAIL, offset ERR_REPEAT_LOGIN
+		invoke crt_strlen, addr @tmpMsg
+		invoke send, sockfd, addr @tmpMsg, eax, 0
 		mov eax, -1
 		ret
 	.endif
 
 	; check whether password right
 	invoke ifPasswordRight, username, password
-	.if eax == 1
-		; repeat login
-		invoke getClientFd, username, addr @tempfd
-		.if eax == 1
-			invoke send, sockfd, addr FAIL_HINT, sizeof FAIL_HINT, 0
-			mov eax, -1
-			ret
-		.endif
-
-		; login success
-		invoke send, sockfd, addr SUCCESS_HINT, sizeof SUCCESS_HINT, 0
-
-		; add new client
-		invoke addNewClient, username, sockfd
-
-		ret
-	.else
-		invoke send, sockfd, addr FAIL_HINT, sizeof FAIL_HINT, 0
+	.if eax == 0
+		invoke crt_sprintf, addr @tmpMsg, offset MSG_FORMAT1, SERVER_FAIL, offset ERR_WRONG_PASS
+		invoke crt_strlen, addr @tmpMsg
+		invoke send, sockfd, addr @tmpMsg, eax, 0
 		mov eax, -1
 		ret
 	.endif
+
+	; login success
+	invoke send, sockfd, addr SUCCESS_HINT, sizeof SUCCESS_HINT, 0
+	invoke addNewClient, username, sockfd
+	ret
 logIn ENDP
 
-
+;--------------------------------------------------------------
 signIn PROC sockfd:dword, username:ptr byte, password:ptr byte
+; user sign in
+;--------------------------------------------------------------
 	; whether already sign in
+	LOCAL @tmpMsg[512]:byte
+
 	invoke ifSignIn, username
 	.if eax == 0
 		invoke writeNewUser, username, password
@@ -531,56 +544,64 @@ signIn PROC sockfd:dword, username:ptr byte, password:ptr byte
 		mov eax, 1
 		ret
 	.else
-		invoke send, sockfd, addr FAIL_HINT, sizeof FAIL_HINT, 0
+		invoke crt_sprintf, addr @tmpMsg, offset MSG_FORMAT1, SERVER_FAIL, offset ERR_REPEAT_SIGNIN
+		invoke crt_strlen, addr @tmpMsg
+		invoke send, sockfd, addr @tmpMsg, eax, 0
 		mov eax, 0
 		ret
 	.endif
 signIn ENDP
 
-
+;--------------------------------------------------------------
 clientConnect PROC sockfd:dword
-	LOCAL @type[10]:byte
-	LOCAL @username[512]:byte
-	LOCAL @password[512]:byte
-	invoke RtlZeroMemory, addr @username, 512
-	invoke RtlZeroMemory, addr @password, 512
-	invoke RtlZeroMemory, addr @type, 10
+; sign in: 0 username password
+; login in: 1 username password
+;--------------------------------------------------------------
+	LOCAL @cursor:dword
+	LOCAL @buffer[512]:byte
+	LOCAL @type:byte
+	LOCAL @username[256]:byte
+	LOCAL @password[256]:byte
 
-	invoke crt_printf, addr NEW_CONNECT_HINT
+	invoke RtlZeroMemory, addr @buffer, 512
+	invoke recv, sockfd, addr @buffer, sizeof @buffer, 0
+	
+	mov al, @buffer
+	mov @type, al
 
-	; connect type
-	invoke recv, sockfd, addr @type, sizeof @type, 0
-	invoke send, sockfd, addr SUCCESS_HINT, sizeof SUCCESS_HINT, 0
+	mov @cursor, addr @buffer
+	add @cursor, 2
+	invoke crt_strstr, @cursor, offset SEP
+	push eax
+	mov [eax], 0
+	invoke crt_strcpy, addr @username, @cursor
+	pop eax
 
-	; username
-	invoke recv, sockfd, addr @username, sizeof @username, 0
-	invoke send, sockfd, addr SUCCESS_HINT, sizeof SUCCESS_HINT, 0
+	mov @cursor, eax
+	inc @cursor
+	invoke crt_strcpy, addr @password, @cursor
 
-	; password 
-	invoke recv, sockfd, addr @password, sizeof @password, 0
-	invoke send, sockfd, addr SUCCESS_HINT, sizeof SUCCESS_HINT, 0
 
-	mov al, @type
-	.if al == CONNECT_LOGIN
+	.if @type == CLIENT_SIGNUP
 		invoke logIn, sockfd, addr @username, addr @password
 		.if eax != -1
 			push eax
-			invoke crt_printf, addr LOGIN_SUCCESS_HINT
+			invoke crt_printf, offset LOGIN_SUCCESS_HINT, addr @username
 			pop eax
 			ret
 		.else
-			invoke crt_printf, addr LOGIN_FAIL_HINT
+			invoke crt_printf, offset LOGIN_FAIL_HINT, addr @username
 			mov eax, -1
 			ret
 		.endif
-	.else
+	.elseif @type == CLIENT_LOGIN
 		invoke signIn, sockfd, addr @username, addr @password
 		.if eax == 1
-			invoke crt_printf, addr SIGNUP_SUCCESS_HINT
+			invoke crt_printf, addr SIGNUP_SUCCESS_HINT, addr @username
 			mov eax, -1
 			ret
 		.else
-			invoke crt_printf, addr SIGNUP_FAIL_HINT
+			invoke crt_printf, addr SIGNUP_FAIL_HINT, addr @username
 			mov eax, -1
 			ret
 		.endif
@@ -596,40 +617,42 @@ main PROC
     LOCAL @stSin:sockaddr_in
 	LOCAL @connSock:dword
 	LOCAL @clientid:dword
+	LOCAL @serverPort:dword
+	LOCAL @listenSocket:dword
 
 	; pick listen port
 	invoke crt_printf, addr BIND_PORT_HINT
-	invoke crt_scanf, addr INPUT_DB_FORMAT, addr serverPort
-    invoke WSAStartup, 101h,addr @stWsa
+	invoke crt_scanf, addr MSG_FORMAT0, addr @serverPort
 
     ; create socket
+	invoke WSAStartup, 101h,addr @stWsa
     invoke socket, AF_INET, SOCK_STREAM,0
     .if eax == INVALID_SOCKET
         invoke MessageBox, NULL, addr ERR_BUILD_SOCKET, addr ERR_BUILD_SOCKET, MB_OK
 		ret
     .endif
-    mov listenSocket, eax
+    mov @listenSocket, eax
 
 	; bind socket
     invoke RtlZeroMemory, addr @stSin,sizeof @stSin
-    invoke htons, serverPort
+    invoke htons, @serverPort
     mov @stSin.sin_port, ax
     mov @stSin.sin_family, AF_INET
     mov @stSin.sin_addr, INADDR_ANY
-    invoke bind,listenSocket, addr @stSin,sizeof @stSin
+    invoke bind, @listenSocket, addr @stSin,sizeof @stSin
     .if eax
 		invoke MessageBox,NULL, addr ERR_BIND_SOCKET, addr ERR_BIND_SOCKET, MB_OK
 		ret
     .endif
 
     ; listen socket
-    invoke listen, listenSocket, BACKLOG
+    invoke listen, @listenSocket, BACKLOG
     invoke crt_printf, addr START_HINT
 
     .while TRUE
 		push ecx
 		; accept new socket
-		invoke accept, listenSocket, NULL, 0
+		invoke accept, @listenSocket, NULL, 0
 		.break .if eax==INVALID_SOCKET
 
 		mov @connSock, eax
@@ -644,7 +667,7 @@ main PROC
         pop ecx
     .endw
 
-    invoke closesocket, listenSocket
+    invoke closesocket, @listenSocket
     ret
 main ENDP
 
